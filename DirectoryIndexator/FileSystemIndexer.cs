a597e2f03fdaaafe8218706fdd9772e3189100b5
Roman Dark <amaii.sakura@gmail.com>
2019-11-22 14:06:49 +0600

    Добавьте файлы проекта.

---
+++ DirectoryIndexator/FileSystemIndexer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DirectoryIndexator
+{
+    public class FileSystemIndexer
+    {
+        public static int cFiles = 0;
+        public static int cFolders = 0;
+        private readonly static object _locker = new object();
+        private readonly static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(3, 3);
+        public static ConcurrentDictionary<string, ICollection<string>> Index { get; set; } =
+            new ConcurrentDictionary<string, ICollection<string>>(10, 100_000);
+        public static void IndexDirectory(DirectoryInfo directory)
+        {
+            _semaphoreSlim.Wait();
+            try
+            {
+                var files = directory.GetFiles();
+                cFolders++;
+
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        if (!Index.ContainsKey(file.Name))
+                        {
+                            lock (_locker)
+                            {
+                                
+                                Index[file.Name] = new List<string>();
+                                Index[file.Name].Add(file.FullName);
+                            }
+                        }
+                        else
+                        {
+                            lock (_locker)
+                            {
+                                
+                                Index[file.Name].Add(file.FullName);
+                            }
+                        }
+                        cFiles++;
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+
+
+                var subDirectories = directory.GetDirectories();
+                if (subDirectories.Any())
+                {
+                    foreach (var dir in subDirectories)
+                    {
+                        try
+                        {
+                            ThreadPool.QueueUserWorkItem(work => IndexDirectory(dir));
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }                
+        }
+    }
+}
---
+++ DirectoryIndexator/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryIndexator
+{
+    public partial class Form1 : Form
+    {
+        //static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        //static int alarmCounter = 1;
+        //static bool exitFlag = false;
+        private int cFiles = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Program Files");
+
+            FileSystemIndexer.IndexDirectory(directory);
+
+            timer1.Enabled = true;
+
+            //ThreadPool.QueueUserWorkItem(work =>
+            //{
+            //    while (timer1.Enabled)
+            //    {
+            //        cFiles = FileSystemIndexer.Index.Count;
+            //    }
+            //});        
+            
+
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             var total = FileSystemIndexer.Index.Select(p => p.Value.Count).Sum();
+
+            if (FileSystemIndexer.Index.ContainsKey(textBox2.Text))
+            {
+                var value = FileSystemIndexer.Index[textBox2.Text];
+                string valueDisplay = string.Empty;
+                value.ToList().ForEach(p => valueDisplay += (p + Environment.NewLine));
+
+                MessageBox.Show(valueDisplay);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text ="Файлов: " + FileSystemIndexer.cFiles + "  Папок: " + FileSystemIndexer.cFolders;
+            if (cFiles < FileSystemIndexer.cFiles)
+            {
+                cFiles = FileSystemIndexer.cFiles;                
+            }             
+            else
+            if (cFiles == FileSystemIndexer.cFiles)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("индексирование файлов завершено");                
+            }
+        }
+    }
+}
